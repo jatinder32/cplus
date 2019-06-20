fd09b6563c6a49c77fcae8d10ceb80cabfb17f9d
Jatinder Kumar <jatinder.kumar@msystechnologies.com>
2019-06-20 20:57:28 +0530

    Initial Commit

---
+++ section13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  section13
+//
+//  Created by MSys on 20/06/19.
+//  Copyright © 2019 MSys. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
